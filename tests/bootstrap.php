deba28c2e671ab2a7dee82bcd29143e91b74302d
mathieu.brunot <mathieu.brunot@monogramm.io>
2020-08-18 01:51:30 +0200

    :green_heart: Backup plugin name retrieval
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,8 +10,9 @@ if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-    $_tests_plugin = getenv('WP_TESTS_PLUGIN');
-    require dirname( __FILE__ ) . '/../' . $_tests_plugin;
+    $_tests_plugin = getenv('WP_PLUGIN');
+    if ( !$_tests_plugin ) $_tests_plugin = dirname( '..' );
+    require dirname( __FILE__ ) . '/../' . $_tests_plugin . '.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
