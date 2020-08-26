81744512508e80031d620b53f8a9bd4c1a1b743f
monogramm <monogramm@monogramm-template.monogramm.io>
2020-08-26 02:16:04 +0200

    unit test, widget added, add role file modified

--- functions.php
+++ functions.php
@@ -4,22 +4,16 @@
  * Create roles and capabilities
  */
 
-function add_roles_on_activation() {
-    add_role('organiser', 'Organiser',
+function wps_add_role() {
+    add_role( 'organiser', 'Organiser',
         array(
             'read' => true,
+            'edit_posts' => true,
             'delete_posts' => true,
-            'edit_posts' => true
         )
     );
- 
 }
+add_action( 'init', 'wps_add_role' );
 
-function del_roles_on_uninstall() {
-    remove_role('organiser');
-}
-
-register_activation_hook( __FILE__, 'add_roles_on_activation' );
-register_uninstall_hook( __FILE__, 'del_roles_on_uninstall' );
 
 ?>
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\TestCase;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
